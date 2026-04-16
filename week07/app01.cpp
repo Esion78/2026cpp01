a3bb0b053af5bba1a50f9f96bcd13fc9b003eb46
Esion <dongwon121322@gmail.com>
2026-04-16 11:51:55 +0900

    week07 stack, heap memory v0.2

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 using namespace std;
-
+//2d array : stack memory & heap memory
 int main()
 {
-	int rowsColumns[3][5];
+	int columns = 0;
+	int* arr[3]; //stack memory
+	cin >> columns;
 	
 	for (int i = 0; i < 3; i++)
+		arr[i] = new int[columns]; //heap memory
+
+	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < columns; j++)
 		{
-			rowsColumns[i][j] = i * j;
+			arr[i][j] = i * j;
 		}
 	}
 	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < columns; j++)
 		{
-			cout << rowsColumns[i][j] << " ";
+			cout << arr[i][j] << " ";
 		}
 		cout << "\n";
 	}
-	cout << "\n";
 
 	return 0;
 }
