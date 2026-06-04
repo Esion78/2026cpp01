64da5ff537f565d11feb5e00aadce601f611098a
Esion <dongwon121322@gmail.com>
2026-06-04 11:38:13 +0900

    week14: aggregation v4.0

---
+++ week14/date.h
@@ -0,0 +1,21 @@
+﻿/**************************************************************
+ * Date 클래스의 인터페이스 파일                              *
+ **************************************************************/
+#ifndef DATE_H
+#define DATE_H
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Date  
+{  
+  private: 
+    int year;
+    int month;  
+    int day;
+  public:  
+    Date(int year, int month, int day);
+    ~Date();                                                        
+    void print() const;                                          
+};
+#endif
---
+++ week14/person.h
@@ -0,0 +1,19 @@
+﻿/**************************************************************
+ * Person 클래스의 인터페이스 파일                            *
+ **************************************************************/
+#ifndef PERSON_H
+#define PERSON_H
+#include "date.h"
+
+// Person 클래스의 정의
+class Person
+{
+  private:
+    string id;
+    Date birthDate;
+  public: 
+    Person(string id, Date birthDate);   
+    ~Person(); 
+    void print() const; 
+};
+#endif
